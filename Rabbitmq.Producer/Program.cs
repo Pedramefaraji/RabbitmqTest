38a1271dcf2a5b6230e8739e7a0dfee3fbbafedd
PF8 <Pedramefaraji@gmail.com>
2021-06-18 00:12:38 +0430

    First Commit

---
+++ Rabbitmq.Producer/Program.cs
@@ -0,0 +1,23 @@
+﻿using RabbitMQ.Client;
+using System;
+
+namespace Rabbitmq.Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+            using var Connection = factory.CreateConnection();
+            using var Channel = Connection.CreateModel();
+
+            DirectExchangePublisher.Publish(Channel);
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
