1a2e5ec8ae4e61832608f7a09d1f0cdea7fdbcc6
PF8 <Pedramefaraji@gmail.com>
2021-06-18 12:59:44 +0430

    all Exchange added

---
+++ Rabbitmq.Producer/HeaderExchangePublisher.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rabbitmq.Producer
+{
+    static class HeaderExchangePublisher
+    {
+        public static void Publish(IModel channel)
+        {
+            var ttl = new Dictionary<string, object>
+            {
+                {"x-message-ttl", 30000 }
+            };
+            channel.ExchangeDeclare("demo-header-exchange", ExchangeType.Headers, arguments: ttl);
+            var count = 0;
+
+            while (true)
+            {
+                var message = new { Name = "Producer", Message = $"Hello! Count: {count}" };
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+                var properties = channel.CreateBasicProperties();
+                properties.Headers = new Dictionary<string, object> { { "account", "update" } };
+
+                channel.BasicPublish("demo-header-exchange", string.Empty, properties, body);
+                count++;
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
